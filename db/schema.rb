a85b4204c44db1a96aeb67c2c4d3261edd0a40f5
Alex Chiu <your.AYC726@gmail.com>
2013-12-10 00:03:18 -0500

    updated activities and restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131208203541) do
+ActiveRecord::Schema.define(version: 20131210022923) do
+
+  create_table "activities", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "locations", force: true do |t|
     t.string   "city"
@@ -27,6 +32,11 @@ ActiveRecord::Schema.define(version: 20131208203541) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "restaurants", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "trips", force: true do |t|
